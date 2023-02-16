cfa9e1366e4b5473029593ec965de43d3f40ffff
dongguaguaguagua <1448424184@qq.com>
2023-02-16 19:51:50 +0800

    third commit

--- rhino.js
+++ rhino.js
@@ -1,47 +1,17 @@
-var currentPackageName = common.getlpparam().packageName;
-//请手动将同方法的hook移至同一个hook代码内，否则后面的不会生效
-if (currentPackageName == 'com.lemon.lv') {
-common.hookMethod('com.lm.components.subscribe.config.UserVipInfo', 'isVipUser', [], function (param) {
-     param.setResult(java.lang.Boolean.valueOf('true'));
-}, function (param) {
-    
-}); 
+var packageName = common.getlpparam().packageName;
 
-common.hookMethod('com.vega.pay.data.VipInfo', 'isVip', [], function (param) {
-     param.setResult(java.lang.Boolean.valueOf('true'));
-}, function (param) {
-    
-}); 
-
-common.hookMethod('com.vega.pay.data.x30_m', 'isVip', [], function (param) {
-     param.setResult(java.lang.Boolean.valueOf('true'));
-}, function (param) {
-    
-}); 
-
-common.hookMethod('com.vega.cloud.upload.model.PurchaseInfo', 'getHasPurchased', [], function (param) {
-     param.setResult(java.lang.Boolean.valueOf('true'));
-}, function (param) {
-    
-}); 
-
-common.hookMethod('com.vega.main.data.PurchaseInfoApi', 'getHasPurchased', [], function (param) {
-     param.setResult(java.lang.Boolean.valueOf('true'));
-}, function (param) {
-    
-}); 
-
-common.hookMethod('com.vega.pay.data.x30_j', 'getHasPurchased', [], function (param) {
-     param.setResult(java.lang.Boolean.valueOf('true'));
-}, function (param) {
-    
-}); 
-
-common.hookMethod('com.bytedance.pangle.Zeus', 'hasInit', [], function (param) {
-     param.setResult(java.lang.Integer.valueOf('0'));
-}, function (param) {
-    
-}); 
-
-
+if (packageName == 'com.flyersoft.moonreader'){
+common.toast('cracked by hzy\npowered by jshook\n')
+common.dialog('脚本仅供学习交流使用，切勿用于商业用途')
+common.hookAllMethods('com.flyersoft.books.A','isPro',function (param) {
+      param.setResult(java.lang.Boolean.valueOf('true'));
+},null);
+common.hookAllMethods('com.flyersoft.components.MrAd','disableAds',function (param) {
+      param.setResult(java.lang.Boolean.valueOf('true'));
+},null);
+// common.hookAllMethods('com.flyersoft.moonreader.ActivityMain','showAdFreeOptions',null,null,function (param) {
+//       common.log(common.thisMethod(param));
+// })
+// common.setStaticObjectField('android.view.View', 'removeAdButton', java.lang.Integer.valueOf('0'));
+// common.getStaticObjectField('com.flyersoft.moonreader.R.id', 'removeAdButton', java.lang.Integer.valueOf('0'));
 }
