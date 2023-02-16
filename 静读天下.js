5f59632c3ae6e38715633f5a83c6b300a5ba8d9e
dongguaguaguagua <1448424184@qq.com>
2023-02-16 19:28:57 +0800

    second commit

--- 静读天下.js
+++ 静读天下.js
@@ -9,9 +9,9 @@ common.hookAllMethods('com.flyersoft.books.A','isPro',function (param) {
 common.hookAllMethods('com.flyersoft.components.MrAd','disableAds',function (param) {
       param.setResult(java.lang.Boolean.valueOf('true'));
 },null);
-common.hookAllMethods('com.flyersoft.moonreader.ActivityMain','showAdFreeOptions',null,null,function (param) {
-      common.log(common.thisMethod(param));
-})
+// common.hookAllMethods('com.flyersoft.moonreader.ActivityMain','showAdFreeOptions',null,null,function (param) {
+//       common.log(common.thisMethod(param));
+// })
 // common.setStaticObjectField('android.view.View', 'removeAdButton', java.lang.Integer.valueOf('0'));
-common.getStaticObjectField('com.flyersoft.moonreader.R.id', 'removeAdButton', java.lang.Integer.valueOf('0'));
+// common.getStaticObjectField('com.flyersoft.moonreader.R.id', 'removeAdButton', java.lang.Integer.valueOf('0'));
 }
